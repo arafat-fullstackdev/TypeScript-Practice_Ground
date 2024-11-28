c404b87c21fa93297ff8185ceae11bd0ef1974ec
Arafat Patwary <farabipsk@gmail.com>
2024-11-28 09:27:00 +0600

    Add TypeScript BootCamp Project

---
+++ beginner/index.js
@@ -0,0 +1,6 @@
+var firstName = "Dimitri";
+var lastName = "Farabi";
+console.log(typeof firstName);
+// console.log(typeof lastName);
+//TS run
+// tsc index.ts - node index.js
---
+++ beginner/index.ts
@@ -0,0 +1,7 @@
+let firstName: string = "Dimitri";
+let lastName: string = "Farabi";
+
+console.log(typeof firstName);
+// console.log(typeof lastName);
+//TS run
+// tsc index.ts - node index.js
