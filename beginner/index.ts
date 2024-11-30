f7a7ade9a4a0b2daca468e0b57b6a97f3b8b090d
Arafat Patwary <farabipsk@gmail.com>
2024-11-30 09:32:23 +0600

    Add kwyword practice

--- beginner/index.ts
+++ beginner/index.ts
@@ -5,3 +5,12 @@ console.log(typeof firstName);
 // console.log(typeof lastName);
 //TS run
 // tsc index.ts - node index.js
+//? Day 2
+//! Keywords Practice //
+let anyType: any = "20";
+anyType = "Let's Begin With TypeScript";
+console.log(anyType);
+
+let unknownType: unknown = "22";
+unknownType = "Unknown";
+// console.log(unknownType);
