eee8b12add552f059a7f58feb978c582c0c821ea
Arafat Patwary <farabipsk@gmail.com>
2024-12-01 16:27:55 +0600

    Use Push methods

--- beginner/array.js
+++ beginner/array.js
@@ -1,2 +1,11 @@
 var numbers = [11, 22, 33, 44, 55, 66];
 console.log(numbers);
+var fruits = ["Apple", "Orange", "Grape"];
+console.log(typeof fruits);
+console.log(fruits);
+//* push() //
+var digit = [10, 20, 30, 40];
+digit.push(50);
+console.log(digit);
+var head = numbers[0];
+console.log(head);
