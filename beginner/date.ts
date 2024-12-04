054f6a333c89df07e45355741bcf014a54a8b59a
Arafat Patwary <farabipsk@gmail.com>
2024-12-04 17:03:57 +0600

    Update date logic

--- beginner/date.ts
+++ beginner/date.ts
@@ -1,11 +1,11 @@
 //?  date //
-const getFormateDate(): string {
-    const now = new Date();
-    const year = now.getFullYear();
-    const month = String(now.getMonth() + 1).padEnd(2, '0');
-    const day = String(now.getDate()).padEnd(2, '0');
+function getFormateDate(): string {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = ("0" + (now.getMonth() + 1)).slice(-2); // Manual padding
+  const day = ("0" + now.getDate()).slice(-2); // Manual padding
 
-    return `${year}-${month}-${day}`;
+  return `${year}-${month}-${day}`;
 }
 
 console.log(getFormateDate());
