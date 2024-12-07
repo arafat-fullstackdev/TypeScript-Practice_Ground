91315434046be2694f594d3f73c949391f57d164
Arafat Patwary <farabipsk@gmail.com>
2024-12-07 07:12:11 +0600

    Use String in Tuple

--- beginner/tuple.js
+++ beginner/tuple.js
@@ -1,10 +1,7 @@
 //* Tuple */
 var myTuple = ["11", 33, "Hello", true];
-// console.log(myTuple[0]);
-// console.log(myTuple[1]);
-// console.log(myTuple[3]);
-// console.log(myTuple[4]);
-//! part 2 //
-var tuple = ["55", "TypeScript"];
-tuple[2] = "yep";
-console.log(tuple);
+var employee = {
+    employeeName: "Dimitri Farabi",
+    employeeJob: "Developer",
+};
+console.log(employee);
