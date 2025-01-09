ff1a8f5255c14fac08a53fd7dbaf23b97edf7321
Arafat Patwary <farabipsk@gmail.com>
2025-01-09 08:21:51 +0600

    Use differ object

---
+++ beginner/union.js
@@ -0,0 +1,15 @@
+// let result: number | string;
+// result = 22;
+// result = "Hello, TypeScript!";
+// console.log(result); 
+//? object 
+var c;
+c = {
+    name: 'Dimitri',
+    age: 2566,
+    location: true
+};
+var a = [];
+a.push(33, 4, 4, 55);
+console.log(a);
+console.log(c);
--- beginner/union.ts
+++ beginner/union.ts
@@ -1,5 +1,22 @@
-let result: number | string;
-result = 22;
-result = "Hello, TypeScript!";
+// let result: number | string;
+// result = 22;
+// result = "Hello, TypeScript!";
 
-console.log(result);
+// console.log(result); 
+
+//? object 
+let c :{
+    name: string,
+    age: number,
+    location:boolean
+};
+c = {
+    name:'Dimitri',
+    age: 2566,
+    location:true
+}
+
+let a : (number | string)[] = [];
+a.push(33,4,4,55);
+console.log(a);
+console.log(c); 
