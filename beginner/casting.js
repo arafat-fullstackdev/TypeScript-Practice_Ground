8c1eca8b5e35ddc76b35be6ec2de6b848f2b7021
Arafat Patwary <farabipsk@gmail.com>
2025-03-22 23:45:24 +0600

    Make casting ts

---
+++ beginner/casting.js
@@ -0,0 +1,8 @@
+// TS Casting
+var myVar = 'Hello, TS';
+var stringLen = myVar.length;
+console.log(myVar);
+console.log(stringLen);
+var myLet = 'Hi,Planet';
+var result = myLet.toString().length;
+console.log(result);
